386d8c7d1ad7f50bf266e4fe9e071f38e3181af8
Yunus Emre Er <yunus_emre_Er34@hotmail.com>
2025-08-06 15:10:09 +0300

    06.08 C# Interfaces

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+PersonelManager personelManager = new PersonelManager();
+List<IPersonManager> personManagers = new List<IPersonManager> {
+    new CustomerManager(),
+    new EmployeeManager()
+};
+
+
+personelManager.Add(personManagers);
+
+interface IPersonManager
+{
+    void Add();
+    void Update();
+}
+
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        System.Console.WriteLine("Musteri Eklendi");
+    }
+
+    public void Update()
+    {
+        System.Console.WriteLine("Musteri Guncellendi");
+    }
+}
+
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        System.Console.WriteLine("Personel Eklendi");
+    }
+
+    public void Update()
+    {
+        System.Console.WriteLine("Personel Guncellendi");
+    }
+}
+
+class PersonelManager
+{
+    public void Add(List<IPersonManager> personManagers)
+    {
+        foreach (var personel in personManagers)
+        {
+            personel.Add();
+        }
+    }
+}
